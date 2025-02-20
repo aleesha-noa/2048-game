561589f2a0c07d14fc36c36211db21d632c13028
Aleesha Katary <akatary@uwaterloo.ca>
2025-02-19 21:04:58 -0500

    Adding global variables

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+import math
+
+# initialize pygame
+pygame.init()
+
+##### GLOBAL VARIABLES #####
+FPS = 60 # frames per second
+
+# want game board to be a square, so HEIGHT == WIDTH
+WIDTH = 800
+HEIGHT = 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+# make outline grey (in RGB)
+OUTLINE_COLOUR = (157, 155, 155)
+
+# choose thickness of outline
+OUTLINE_THICKNESS = 10
+
+# set background and font colour
+BACKGROUND_COLOUR = (205, 110, 101)
+FONT_COLOUR = (119, 110, 101)
+
+# use this to render text onto the screen
+#TODO: change font
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VEL = 20 # speed at which tiles will move in pixels per second
+
+# make the display window in pygame
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
+
+##### METHODS #####
+def main(window):
+    clock = pygame.time.Clock() # regulate the speed of the loop
+    run = True
+
+pygame.quit()
+
+
+if __name__ == "__main__":
+    main(WINDOW) # running the game on the specified window
